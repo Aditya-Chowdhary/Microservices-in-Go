819e950079cd2e8ec7f1bae3a34c1ee8ce0c9431
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-04-17 21:31:23 +0530

    2.1 Movie Metadata Service

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Aditya-Chowdhary/microservices-go
+
+go 1.21.3
---
+++ metadata/internal/handler/http/http.go
@@ -0,0 +1,41 @@
+package http
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/Aditya-Chowdhary/microservices-go/metadata/internal/controller/metadata"
+	"github.com/Aditya-Chowdhary/microservices-go/metadata/internal/repository"
+)
+
+type Handler struct {
+	ctrl *metadata.Controller
+}
+
+func New(ctrl *metadata.Controller) *Handler {
+	return &Handler{ctrl}
+}
+
+func (h *Handler) GetMetadata(w http.ResponseWriter, r *http.Request) {
+	id := r.FormValue("id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	ctx := r.Context()
+	m, err := h.ctrl.Get(ctx, id)
+	if err != nil && errors.Is(err, repository.ErrNotFound) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	} else if err != nil {
+		log.Printf("Repository got error: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(m); err != nil {
+		log.Printf("Response encode error: %v\n", err)
+	}
+}
